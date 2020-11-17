8fe1de3dc3388094d6a37a09ae8f7fe962799d93
KarnaukhVeronica <62516559+KarnaukhVeronica@users.noreply.github.com>
2020-11-17 23:51:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,21 +10,5 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-    DBManager db;
-    vector <Task> item1( db.GetTask( QDateTime::currentDateTime().addDays(-50), QDateTime::currentDateTime().addDays(50) ) );
-
-    qDebug() << "BEFORE ACT: wt=" << item1[0].getWorkTime() << " dt=" << item1[0].getDate();
-    item1[0].Activate();
-    qDebug() << "AFTER  ACT: wt=" << item1[0].getWorkTime() << " dt=" << item1[0].getDate();
-    db.ActivateTask(item1[0]);
-
-    QThread::sleep( 62 );
-
-    qDebug() << "BEFORE DEACT: wt=" << item1[0].getWorkTime() << " dt=" << item1[0].getDate();
-    item1[0].Deactivate();
-    qDebug() << "AFTER  DEACT: wt=" << item1[0].getWorkTime() << " dt=" << item1[0].getDate();
-    db.DeactivateTask(item1[0]);
-
     return a.exec();
 }
